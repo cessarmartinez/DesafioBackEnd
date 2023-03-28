b3e059e719e0271ae4679529a25385aa04164a11
cessarmartinez <103976412+cessarmartinez@users.noreply.github.com>
2023-03-28 08:39:06 -0300

    -

--- ProductManager.js
+++ ProductManager.js
@@ -1,47 +1,112 @@
-let productos = []
+let products = [];
+
+const fs = require ('fs');
+
 class ProductManager {
-    constructor (){
-        this.product = productos
+    constructor(){
+        this.products = products
+        this.path = './data.json'
     }
-    addProducts(newProduct){
-        if(!newProduct.title || 
-            !newProduct.description || 
-            !newProduct.price || 
-            !newProduct.thumbnail || 
-            !newProduct.code || 
-            !newProduct.stock) return 'Todos los campos son necesarios'
-        
-        let products = this.product.find(prod => prod.code == newProduct.code)
-        if(products) return 'Un producto con este código ya fue ingresado'
-        return this.product.push({id: this.product.length+1,...newProduct})
+
+    archivoJson = async () => {
+        try {
+            const toJson = JSON.stringify(this.products, 'null', 2)
+            await fs.promises.writeFile(this.path, toJson, 'utf-8')
+        }
+        catch (err) { return console.log(err) }
     }
-    getProducts(){
-        return this.product
+
+    addProducts = (title, description, price, thumbnail, code, stock) => {
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        if(this.products.length === 0){
+            product.id = 1
+        } else {
+            product.id = this.products[this.products.length - 1].id + 1
+        }
+
+        if (Object.values(product).every(value => value)){
+            this.products.push(product)
+            this.archivoJson()
+        } else {
+            return console.log('Todos los campos son obligatorios')
+        }
     }
-    getProductsById(id){
-        let product = this.product.find(prod => prod.id == id)
-        if (!product) return 'Not Found'
-        return product
+
+    getProducts = async () => {
+        try {
+            const readFile = await fs.promises.readFile(this.path, 'utf-8');
+            return console.log(readFile)
+        }
+        catch (err) { return console.log(err) }
+    }
+
+    getProductsById = async (id) => {
+        try {
+            const readFile = await fs.promises.readFile(this.path, 'utf-8')
+            const obj = JSON.parse(readFile)
+            const find = obj.find(product => product.id === id)
+            return find ? find : console.log('No products found')
+        }
+        catch (err) { return console.log(err) }
+    }
+
+    updateProduct = async (id, obj) => {
+        try {
+            const readFile = await fs.promises.readFile(this.path, 'utf-8')
+            const products = JSON.parse(readFile)
+
+            const returnObj = Object.assign(products[id-1], obj)
+            console.log(products[id-1])
+            this.products = products
+            this.archivoJson()
+        }
+        catch (err) { return console.log(err) }
+    }
+
+    deleteProduct = async (id) => {
+        try { 
+            const readFile = await fs.promises.readFile(this.path, 'utf-8')
+            const products = JSON.parse(readFile)
+            console.log(products.splice(id-1, 1), 'Producto eliminado')
+            this.products = products
+            this.archivoJson()
+        }
+        catch (err) { return console.log(err) }
     }
 }
 
-const product = new ProductManager();
 
-product.addProducts({
-    title: 'hProducto',
-    description: "descripción",
-    price: 1500,
-    thumbnail: 'link',
-    code: 001,
-    stock: 12
-})
-console.log(product.addProducts({
-    title: 'Producto 2',
-    description: "descripción",
-    price: 1500,
-    thumbnail: 'link',
-    code: 34,
-    stock: 12
-}))
+const product = new ProductManager()
+
+product.addProducts(
+    'Producto',
+    'Descripcion',
+    1500,
+    'thumbnail',
+    001,
+    6
+);
+product.addProducts(
+    'Producto',
+    'Descripcion',
+    1500,
+    'thumbnail',
+    002,
+    6
+);
+
+
+
 console.log(product.getProducts());
-console.log(product.getProductsById());
+
+//console.log(product.getProductsById(1))
+
+//product.deleteProduct();
