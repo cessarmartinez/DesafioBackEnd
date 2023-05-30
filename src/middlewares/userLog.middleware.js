fdff16b43afc276ffa28891e2cd4f6e3254f9877
cessarmartinez <103976412+cessarmartinez@users.noreply.github.com>
2023-05-30 01:36:54 -0300

    -

--- src/middlewares/userLog.middleware.js
+++ src/middlewares/userLog.middleware.js
@@ -1,6 +1,5 @@
 function userLog (req, res, next) {
     if (!req.session?.user) {
-        //No tengo user, retorno Error
         return res.status(401).send("Usuario No logueado")
     }
     next()
