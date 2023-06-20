c891cc1e3961b552d64f4107d73a4fc989946083
cessarmartinez <103976412+cessarmartinez@users.noreply.github.com>
2023-06-20 16:12:33 -0300

    -

--- src/middlewares/auth.middleware.js
+++
@@ -1,8 +0,0 @@
-function auth(req, res, next) {
-    if (req.session?.user!='jr' || !req.session?.admin) {
-        return res.status(401).send("Operación no permitida para su nivel de usuario")
-    }
-    next()
-}
-
-module.exports = {auth} 
--- src/middlewares/userLog.middleware.js
+++
@@ -1,8 +0,0 @@
-function userLog (req, res, next) {
-    if (!req.session?.user) {
-        return res.status(401).send("Usuario No logueado")
-    }
-    next()
-}
-
-module.exports = userLog
